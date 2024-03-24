62468e87c33b073033f447d5a011e313d515e647
Prompt Engineer <prompt.engineer48@gmail.com>
2024-03-24 16:32:51 +0530

    Update client.py

--- client.py
+++ client.py
@@ -1,31 +1,29 @@
 from langserve import RemoteRunnable
 from langchain_core.output_parsers import StrOutputParser
 
-llm = RemoteRunnable("http://localhost:8001/basic_chat/") | StrOutputParser()
-# for token in llm.stream("Hello World! How is it going?"):
-#     print(token, end="")
+llm = RemoteRunnable("http://localhost:8002/basic_chat/") | StrOutputParser()
 
 import gradio as gr
 
 
-def chat_stream(message):
-    buffer = ""
-    for token in llm.stream(message):
-        buffer += token
-        yield buffer
+def rhyme_chat(message, history):
+    return llm.invoke(message)
 
 
-gr.ChatInterface(chat_stream).queue().launch(share=False, debug=True)
+gr.ChatInterface(rhyme_chat).launch()
+
+
+# def chat_stream(message):
+#     buffer = ""
+#     for token in llm.stream(message):
+#         buffer += token
+#         yield buffer
+
+# gr.ChatInterface(chat_stream).queue().launch(share=False, debug=True)
 
 
 # ######################################################
 # # Non-streaming Interface like that shown above
 
-
-# def rhyme_chat(message, history):
-#     return llm.invoke(message)
-
-
-# gr.ChatInterface(rhyme_chat).launch()
 
 # ######################################################
